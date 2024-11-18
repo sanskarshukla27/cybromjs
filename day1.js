977a84cdea3e58ce263533760d939368da80f5c6
sanskarshukla27 <97541973+sanskarshukla27@users.noreply.github.com>
2024-11-18 20:03:54 +0530

    alert

--- day1.js
+++ day1.js
@@ -1,15 +1,62 @@
 
-function run(){
-    var num=25;
-    console.log(num)
+//function run(){
+    // var num=25;
+    // console.log(num)
 
-    let num1 =55;
-    console.log(num1)
+    // let num1 =55;
+    // console.log(num1)
 
-    const num2=60;
-    console.log(num2)
+    // const num2=60;
+    // console.log(num2)  
+
+
+    //====================scope==============
+    // const num =55;
+    // {
+    //     const num = 45;
+    // }
+    // console.log(num)
+
+    //=======================redeclaration==================
+
+    // const num=44;
+    // const num=55;
+    // console.log(num) 
+
+
+    //========================reassisment==============
+    // const num=45;
+    // num = 34;
+    // console.log(num)
+
+
+
+
+//}
+//run();
+
+function oper(){
+
+
+    //Alert promt confirm
+
+//alert("hello i am alert")
+//prompt("hello i am promt")
+
+//confirm("hello i am confirm")
+
+let num1 =prompt("enter the num1" )
+let num2 =prompt("enter the num2" )
+
+//console.log(num1+num2)
+//alert ("the sum is : " + (num1+num2))
+alert("the sum of promt is: " + (num1+num2))
+
+
+
+
 }
-run();
+oper();
 
 
 
