1ff3cc16c3227ee53e94a15232b35cbdc136f4c9
sanskarshukla27 <97541973+sanskarshukla27@users.noreply.github.com>
2024-11-27 20:10:49 +0530

    templatelaterals

--- string.js
+++ string.js
@@ -1,6 +1,6 @@
-const letsgo = ()=>{
+// const letsgo = ()=>{
    
-    let text = "hey babe how r uh..:";
+    //let text = ` hey "babe"  'how' r uh..:`; //templatelaterals (`) this is used to add quma double quotes , for multilines,for writingfg expressions
     // let text1 = "i love you"
     // console.log(text.length);
     // console.log(text.slice(1,6));
@@ -9,6 +9,23 @@ const letsgo = ()=>{
     // console.log(text.replace("hey","hula"));  //here we can use g and i for changing every same words 
     // console.log(text.toLowerCase(text));
     // console.log(text.toUpperCase(text));
-    console.log(text.trim());       //this will remove the space from the starting and end
-}
-letsgo();
+  //  console.log(text.trim());       //this will remove the space from the starting and end
+
+  let s = 10;
+  let s1 =30;
+  let text = `the sum of ${s} + ${s1} is ${s + s1} ` //tmeplatelaterals can be used like this also
+  console.log(text)
+
+// console.log(text)
+
+// }
+// letsgo();
+
+
+//new funtion its third type .....its imidiate call isme call nhi krna pdta (iife)
+
+(function (){
+
+    console.log("hello world")
+
+}())
