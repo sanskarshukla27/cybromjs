8aae53f6cfd53c77c8ebecb340fc6640d7dddf53
sanskarshukla27 <97541973+sanskarshukla27@users.noreply.github.com>
2024-12-04 20:34:41 +0530

    object

---
+++ object.js
@@ -0,0 +1,26 @@
+// let objectName ={
+
+//     key1:"value",
+//     key1:"value2"
+// }
+
+let student ={
+    name:"sanskar",
+    age:21,
+    adress:"bhopal",
+    welcome:function(){
+        return `hello my name is ${this.name} and name is ${this.age} `
+    }
+}
+console.log(student.name);                //how to access
+
+student.gender="male";
+student.email="sanskar2002@gmail.com"     //how to add
+console.log(student);
+
+student.age =23;                          //how to update
+console.log(student);
+
+
+
+console.log(student.welcome())
